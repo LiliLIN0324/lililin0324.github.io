63dde68af9ef220b3167f399d10383ca75529129
Jujubeii <lily1326685527@gmail.com>
2026-02-07 23:15:20 +0900

    feat: 添加多个地图相关组件和功能
    新增热力图、照片地理定位、微信小游戏、开放街道地图、集群可视化等组件
    添加图标库和地图工具类
    实现地图交互、图层控制、3D/2D切换等功能
    优化UI和用户体验

--- docs/assets/icons-DKBzPbYf.js
+++ docs/assets/icons-DKBzPbYf.js
@@ -1,4 +1,4 @@
-import{r as n}from"./vendor-B830Bftk.js";/**
+import{r as n}from"./vendor-BZFHMnVe.js";/**
  * @license lucide-react v0.561.0 - ISC
  *
  * This source code is licensed under the ISC license.
